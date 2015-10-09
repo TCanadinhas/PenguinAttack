c449dbf7af65b3b9e1a963e69425793bddc96d31
Talita Canadinhas <talitacanadinhas@gmail.com>
2015-10-09 10:12:52 -0300

    Versão inicial

---
+++ PenguimAttack/Assets/Scripts/fishController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fishController : MonoBehaviour {
+
+	private int velocity;
+
+	// Use this for initialization
+	void Start () 
+	{
+		velocity = 2;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (this.transform.position.x < 4f)
+		{
+			print("right");
+			if(Input.GetKeyDown(KeyCode.RightArrow))
+				transform.position += new Vector3(velocity * Time.deltaTime,0,0);
+		}
+
+		if (this.transform.position.x > -4f)
+		{
+			if(Input.GetKeyDown(KeyCode.LeftArrow))
+				transform.position -= new Vector3(velocity * Time.deltaTime,0,0);
+		}
+	}
+}
---
+++ PenguimAttack/Assets/Scripts/penguinBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class penguinBehaviour : MonoBehaviour {
+	
+	private float velocity;
+	private GameObject end;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		velocity = 6f;
+		end = GameObject.Find("End");	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		this.transform.position -= new Vector3 (0, velocity * Time.deltaTime, 0);
+		if (this.gameObject.transform.position.y < end.transform.position.y)
+			Destroy (this.gameObject);
+	}
+}
